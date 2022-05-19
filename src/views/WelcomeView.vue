a010209836c00a7095a7658d73a26753ae930117
Ivan Sukhanov <ivan.sukhanov@noveogroup.com>
2022-05-19 19:03:30 +0300

    Added libs

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="welcome">
-    <h2>Welcome to Secret Chat</h2>
-    <span>
-      Create a room and invite your friend. All messages will be deleted after
-      both of you leave a room.
-    </span>
+    <div>
+      <h2>Welcome to Secret Chat</h2>
+      <span>
+        Create a room and invite your friend. All messages will be deleted after
+        both of you leave a room.
+      </span>
+    </div>
+    <router-link :to="`/room/${roomId}`" class="welcome--enter-room">
+      <a-button type="primary"> Enter room </a-button>
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+import { nanoid } from 'nanoid';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'WelcomeView',
+  setup() {
+    return {
+      roomId: nanoid(),
+    };
+  },
 });
 </script>
+
+<style lang="scss" scoped>
+.welcome {
+  &--enter-room {
+    display: block;
+    margin-top: 32px;
+  }
+}
+</style>
